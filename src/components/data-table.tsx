6b29e6128432cd8eb478e1f1d2ed76f039f08169
Piyush Chauhan <piyushchauhan2011@gmail.com>
2023-06-26 20:08:48 +0700

    fix: tick and mark icon colors

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -4,7 +4,8 @@ function getBooleanIcon(value: boolean) {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
-        className="w-5 h-5 text-green-500"
+        fill="currentColor"
+        className="w-5 h-5 text-green-600"
       >
         <path
           fillRule="evenodd"
@@ -19,7 +20,7 @@ function getBooleanIcon(value: boolean) {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
-        className="w-5 h-5 text-red-500"
+        className="w-5 h-5 text-red-600"
       >
         <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
       </svg>
