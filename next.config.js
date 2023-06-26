dde3290923a6f7e64d0ab072dbfa576bc6bd6890
Piyush Chauhan <piyushchauhan2011@gmail.com>
2023-06-27 05:50:19 +0700

    feat: remove sentry integration (#5)
    * feat: remove sentry integration
    
    * feat: fix bullet list and change mdx to md for rendering

--- next.config.js
+++ next.config.js
@@ -5,45 +5,23 @@ const nextConfig = {
     mdxRs: true,
     webVitalsAttribution: ["CLS", "LCP"],
   },
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
 };
 
-const withMDX = require("@next/mdx")();
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
 
 module.exports = withMDX(nextConfig);
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-);
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,12 @@
-import HelloWorld from "../hello.mdx";
+import HelloWorld from "../hello.md";
 
 async function Blog() {
   return (
     <main className="container mx-auto px-6 lg:w-1/2 py-8">
       <HelloWorld />
+      {/* <ul className="hidden list-disc list-inside">
+        <li className=""></li>
+      </ul> */}
     </main>
   );
 }
